f24bf19ca497fb684fb1d122380e816670752115
bharatchhajer <bharatchhajer.work@gmail.com>
2025-06-10 19:55:36 +0530

    made bucket name unique

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "google" {
   region  = var.location
 }
 
-resource "google_storage_bucket" "my_bucket" {
+resource "google_storage_bucket" "practice_my_bucket_bharat_chhajer" {
   name          = var.bucket_name
   location      = var.location
   force_destroy = true
