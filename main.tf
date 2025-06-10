4fc46d6e1203ab82145338a5095fff470bf47af9
bharatchhajer <bharatchhajer.work@gmail.com>
2025-06-10 17:34:47 +0530

    modified main.tf to remove third line in provider google

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.location
-  credentials = file("credentials.json")
+  project = var.project_id
+  region  = var.location
 }
 
 resource "google_storage_bucket" "my_bucket" {
